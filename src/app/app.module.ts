88b52251dcf768439edd279af7a70a305f3de5f1
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-30 14:41:09 +0100

    kirby

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { JiggleTextComponent } from './jiggle-text/jiggle-text.component';
 import { TableCellsColorTransitionComponent } from './table-cells-color-transition/table-cells-color-transition.component';
 import { ChooChooTrainComponent } from './choo-choo-train/choo-choo-train.component';
+import { KirbyComponent } from './kirby/kirby.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JiggleTextComponent,
     TableCellsColorTransitionComponent,
-    ChooChooTrainComponent
+    ChooChooTrainComponent,
+    KirbyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/kirby/kirby.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kirby',
+  templateUrl: './kirby.component.html',
+  styleUrls: ['./kirby.component.scss']
+})
+export class KirbyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
