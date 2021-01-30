19ec127c80163a974e1af54be866f4afd2206e8a
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-30 12:06:22 +0100

    refactore to scss

--- src/app/jiggle-text/jiggle-text.component.ts
+++ src/app/jiggle-text/jiggle-text.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-jiggle-text',
   templateUrl: './jiggle-text.component.html',
-  styleUrls: ['./jiggle-text.component.css']
+  styleUrls: ['./jiggle-text.component.scss']
 })
 export class JiggleTextComponent implements OnInit {
 
--- src/app/table-cells-color-transition/table-cells-color-transition.component.ts
+++ src/app/table-cells-color-transition/table-cells-color-transition.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-table-cells-color-transition',
   templateUrl: './table-cells-color-transition.component.html',
-  styleUrls: ['./table-cells-color-transition.component.css']
+  styleUrls: ['./table-cells-color-transition.component.scss']
 })
 export class TableCellsColorTransitionComponent implements OnInit {
 
