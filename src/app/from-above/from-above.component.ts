a12fd788d05ba2a521d6755a798bc63cc314598d
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-30 18:04:31 +0100

    from above

--- src/app/from-above/from-above.component.ts
+++ src/app/from-above/from-above.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-from-above',
   templateUrl: './from-above.component.html',
-  styleUrls: ['./from-above.component.css']
+  styleUrls: ['./from-above.component.scss']
 })
 export class FromAboveComponent implements OnInit {
 
