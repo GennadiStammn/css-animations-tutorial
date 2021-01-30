ce58986810337e03fdd873f9e5275bfda1ce8e9c
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-30 22:15:04 +0100

    tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { JiggleTextComponent } from './jiggle-text/jiggle-text.component';
-import { TableCellsColorTransitionComponent } from './table-cells-color-transition/table-cells-color-transition.component';
-import { ChooChooTrainComponent } from './choo-choo-train/choo-choo-train.component';
-import { KirbyComponent } from './kirby/kirby.component';
-import { PictureGridComponent } from './picture-grid/picture-grid.component';
-import { SpinTransitionComponent } from './spin-transition/spin-transition.component';
-import { FromAboveComponent } from './from-above/from-above.component';
-import { BubblesComponent } from './bubbles/bubbles.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    JiggleTextComponent,
-    TableCellsColorTransitionComponent,
-    ChooChooTrainComponent,
-    KirbyComponent,
-    PictureGridComponent,
-    SpinTransitionComponent,
-    FromAboveComponent,
-    BubblesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
